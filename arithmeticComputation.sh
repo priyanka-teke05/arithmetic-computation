2408edf7162a02c543accf90579f8cec39601578
priyanka-teke05 <priyankateke1@gmail.com>
2020-08-16 21:16:44 +0530

    ADDED: store expressions result in dictionary

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -3,11 +3,14 @@
 #display welcome message
 echo "Welcome to Arithmetic Compuation and Sorting"
 
+#dictionary
+declare -A expressionsResult
+
 #read three numbers
 read -p "Enter Three Numbers : " a b c
 
 #execute arithmetic operations
-expression1_result=$(echo "scale=2; $a+$b*$c" | bc )
-expression2_result=$(echo "scale=2; $a*$b+$c" | bc )
-expression3_result=$(echo "scale=2; $c+$a/$b" | bc )
-expression4_result=$(($a%$b+$c))
+expressionsResult[expression1_result]=$(echo "scale=2; $a+$b*$c" | bc )
+expressionsResult[expression2_result]=$(echo "scale=2; $a*$b+$c" | bc )
+expressionsResult[expression3_result]=$(echo "scale=2; $c+$a/$b" | bc )
+expressionsResult[expression4_result]=$(($a%$b+$c))
