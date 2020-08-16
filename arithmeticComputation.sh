25c85a492ddaf9c8a57ad361dabd21cb45327a45
priyanka-teke05 <priyankateke1@gmail.com>
2020-08-16 21:23:44 +0530

    ADDED: store expression result into array from dictionary

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -3,14 +3,23 @@
 #display welcome message
 echo "Welcome to Arithmetic Compuation and Sorting"
 
+#constants
+readonly TOTAL_EXPRESSION=4
+
 #dictionary
 declare -A expressionsResult
 
 #read three numbers
 read -p "Enter Three Numbers : " a b c
 
-#execute arithmetic operations
+#execute arithmetic operations and store result in array
 expressionsResult[expression1_result]=$(echo "scale=2; $a+$b*$c" | bc )
 expressionsResult[expression2_result]=$(echo "scale=2; $a*$b+$c" | bc )
 expressionsResult[expression3_result]=$(echo "scale=2; $c+$a/$b" | bc )
 expressionsResult[expression4_result]=$(($a%$b+$c))
+
+#store result in array
+for(( i=0;i<TOTAL_EXPRESSION;i++))
+do
+	eval resultArray[$i]=${expressionsResult[expression$((i+1))_result]}
+done
