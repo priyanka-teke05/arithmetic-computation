c00a7abad38cec0f0edaa53e50089a11e6ee7ffb
priyanka-teke05 <priyankateke1@gmail.com>
2020-08-16 19:33:45 +0530

    ADDED: execute arithmetic operation 4

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -6,6 +6,8 @@ echo "Welcome to Arithmetic Compuation and Sorting"
 #read three numbers
 read -p "Enter Three Numbers : " a b c
 
+#execute arithmetic operations
 expression1_result=$(echo "scale=2; $a+$b*$c" | bc )
 expression2_result=$(echo "scale=2; $a*$b+$c" | bc )
 expression3_result=$(echo "scale=2; $c+$a/$b" | bc )
+expression4_result=$(($a%$b+$c))
